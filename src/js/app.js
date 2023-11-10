880a8a9bdca341951c6f4263f9b0ce88510b1a92
Andrew Ensley <aensley@users.noreply.github.com>
2023-11-10 16:21:03 -0600

    fix(thumbs): fix thumbnail upload

--- src/js/app.js
+++ src/js/app.js
@@ -24,8 +24,6 @@ import { isImage, isVideo } from '../ts/file.ts'
   }
 
   const escapeElement = createElement('textarea')
-  const downloadIcon =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download me-1" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>'
   const deleteIcon =
     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill me-1" viewBox="0 0 16 16"><path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/></svg>'
 
@@ -75,13 +73,7 @@ import { isImage, isVideo } from '../ts/file.ts'
             createElement(
               'td',
               // File name and download link.
-              '<a href="/files/' +
-                encodeURIComponent(file.key) +
-                '">' +
-                downloadIcon +
-                ' ' +
-                escapeHtml(file.key) +
-                '</a>'
+              '<a href="/files/' + encodeURIComponent(file.key) + '">' + escapeHtml(file.key) + '</a>'
             )
           )
           // File size.
@@ -161,15 +153,14 @@ import { isImage, isVideo } from '../ts/file.ts'
    * @param {File} file The file to upload a thumbnail for.
    */
   const uploadThumb = async (file) => {
-    let thumbnailData = ''
+    let thumbnailData
     if (isImage(file.name)) {
       thumbnailData = await generateImageThumbnail(file, 100, 75)
     } else if (isVideo(file.name)) {
       thumbnailData = await generateVideoThumbnail(file, 100, 75)
     }
 
-    if (thumbnailData.length > 0) {
-      console.log(thumbnailData)
+    if (typeof thumbnailData !== 'undefined') {
       const thumbUploadRequest = new XMLHttpRequest()
       thumbUploadRequest.addEventListener('load', () => {
         console.log(thumbUploadRequest.status, thumbUploadRequest.responseText)
